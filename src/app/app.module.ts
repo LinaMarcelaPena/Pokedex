73d4f32b77dd9ffa6d042eeabfd39c5863f6fedc
LinaMarcelaPena <linapena112@gmail.com>
2022-12-02 00:55:22 -0500

    Consumo de API REST

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GeneralComponent } from './general/general.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeneralComponent
   ],
   imports: [
-    BrowserModule
+    //activar modulos que se requieren en el proyecto
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
